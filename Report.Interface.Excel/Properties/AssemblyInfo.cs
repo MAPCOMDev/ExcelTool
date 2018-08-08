fd49569460ad38fa918dc0aad137075091607deb
MAPCOM <dev@mapcom.co.jp>
2018-08-08 15:31:49 +0900

    no message

--- Report.Interface.Excel/Properties/AssemblyInfo.cs
+++ Report.Interface.Excel/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("出簡調Excelツール.拡張DLL")]
+[assembly: AssemblyTitle("Excelツール.拡張DLL")]
 [assembly: AssemblyDescription("Report.Interface.Excel.[データ名]")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MAPCOM")]
-[assembly: AssemblyProduct("出簡調Excelツール.拡張DLL")]
+[assembly: AssemblyProduct("Excelツール.拡張DLL")]
 [assembly: AssemblyCopyright("MAPCOM")]
 [assembly: AssemblyTrademark("MAPCOM")]
 [assembly: AssemblyCulture("")]
